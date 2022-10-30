8ffe70ebee5d6d09f31db7e81314667ad4b7fa74
aameiwei <923070686@qq.com>
2022-10-30 15:23:40 +0800

    20221030 将密码模式生成的token存入redis

---
+++ src/main/java/com/huo/springsecurityoauth2demo/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.huo.springsecurityoauth2demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * @Author 小源同学
+ * @Date 2022 01 16 00 26
+ * @Describe Redis 配置类
+ **/
+@Configuration
+public class RedisConfig {
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public TokenStore redisTokenStore(){
+        return new RedisTokenStore(redisConnectionFactory);
+    }
+}
