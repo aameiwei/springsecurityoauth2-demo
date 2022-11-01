b9ee4140322806714fdaf991e025510cb7c0dbf1
aameiwei <923070686@qq.com>
2022-11-01 09:43:50 +0800

    20221031 配置sso的认证端

--- src/main/java/com/huo/springsecurityoauth2demo/config/AuthorizationServerConfig.java
+++ src/main/java/com/huo/springsecurityoauth2demo/config/AuthorizationServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -76,19 +77,28 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 //配置访问token的有效期
 //                .accessTokenValiditySeconds(60 * 60)
                 //配置redirect-url,用于授权成功后跳转
-                .redirectUris("http://www.baidu.com")
+                //.redirectUris("http://www.baidu.com")
+                .redirectUris("http://localhost:8081/login")
                 //配置申请的权限范围
                 .scopes("all")
                 //accesstoken失效时间
                 .accessTokenValiditySeconds(600)
                 //refreshtoken失效时间
                 .refreshTokenValiditySeconds(88888)
+                //设置自动授权
+                .autoApprove(true)
                 //配置grant-type，授权类型，【授权码模式】
                 //.authorizedGrantTypes("authorization_code")
                 //配置grant-type，授权类型，【密码模式】
                 //refresh_token刷新令牌
                 .authorizedGrantTypes("password","authorization_code","refresh_token")
+        ;
+    }
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //获取密钥需要身份认证，使用单点登录时候必须配置
+        security.tokenKeyAccess("isAuthenticated()")
         ;
     }
 }
